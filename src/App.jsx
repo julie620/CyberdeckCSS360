4d37b4b06cc1afe1429c0cebd3096037ba3c6f2a
JSerr <juliana.chanel@gmail.com>
2026-05-17 20:53:48 -0700

    Reformat using Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
-import Playback from './pages/Playback'
-import PlaylistBrowser from './pages/PlaylistBrowser'
+import { Routes, Route } from "react-router-dom";
+import Playback from "./pages/Playback";
+import PlaylistBrowser from "./pages/PlaylistBrowser";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <Route path="/" element={<Playback />} />
       <Route path="/browse" element={<PlaylistBrowser />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
